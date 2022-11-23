ff9bd6bf83f37e1e7087b0b4369cba9347db74b9
Bartek Banaś <bartek.banas675@gmail.com>
2022-11-23 20:27:13 +0100

    AddToVector

--- Optimization labs/VectorUtilities.cpp
+++ Optimization labs/VectorUtilities.cpp
@@ -7,6 +7,11 @@ std::vector<double> AddVectors(std::vector<double> vector1, std::vector<double> 
     return std::vector<double> {vector1[0]+vector2[0], vector1[1]+vector2[1]};
 }
 
+std::vector<double> AddToVector(std::vector<double> vector, double number)
+{
+    return std::vector<double> {vector[0] + number, vector[1] + number};
+}
+
 std::vector<double> SubtractVectors(std::vector<double> vector1, std::vector<double> vector2)
 {
     return std::vector<double> {vector1[0]-vector2[0], vector1[1]-vector2[1]};
--- Optimization labs/VectorUtilities.h
+++ Optimization labs/VectorUtilities.h
@@ -5,6 +5,7 @@ class VectorUtilities
 {
 public:
     std::vector<double> AddVectors(std::vector<double> vector1, std::vector<double> vector2);
+    std::vector<double> AddToVector(std::vector<double> vector, double number);
     std::vector<double> SubtractVectors(std::vector<double> vector1, std::vector<double> vector2);
     std::vector<double> MultiplyVector(std::vector<double> vector, double multiplier);
 };
