ffa14c31e5945a1681eb9637bc353c375825a9d4
Bartek Banaś <bartek.banas675@gmail.com>
2022-11-24 12:17:43 +0100

    Questionable progress

--- Optimization labs/VectorUtilities.cpp
+++ Optimization labs/VectorUtilities.cpp
@@ -2,12 +2,12 @@
 
 #include <vector>
 
-std::vector<double> AddVectors(std::vector<double> vector1, std::vector<double> vector2)
+static std::vector<double> AddVectors(std::vector<double> vector1, std::vector<double> vector2)
 {
     return std::vector<double> {vector1[0]+vector2[0], vector1[1]+vector2[1]};
 }
 
-std::vector<double> AddToVector(std::vector<double> vector, double number)
+static std::vector<double> AddToVector(std::vector<double> vector, double number)
 {
     return std::vector<double> {vector[0] + number, vector[1] + number};
 }
