d71f0fe6a319add8749ed5da8a66541666b6d058
Bartek Banaś <bartek.banas675@gmail.com>
2022-11-24 13:43:04 +0100

    Fix up of .h file

---
+++ VectorUtilities.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include <vector>
+
+std::vector<double> AddVectors(std::vector<double> vector1, std::vector<double> vector2);
+std::vector<double> AddToVector(std::vector<double> vector, double number);
+std::vector<double> SubtractVectors(std::vector<double> vector1, std::vector<double> vector2);
+std::vector<double> MultiplyVector(std::vector<double> vector, double multiplier);
+void PrintVector(std::vector<double> vector);
+
