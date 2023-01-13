65a801d5734f790d294b47277ad399ed55bbba6a
Tomasz Banaś <bartek.banas675@gmail.com>
2023-01-13 10:10:15 +0100

    Clean up

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -9,8 +9,6 @@
 
 using namespace std;
 
-;
-
 vector<double> nelderMead(double objectiveFunction(std::vector<double>), const std::vector<double>& initialPoint,
                           double alpha, double gamma, double rho,
                           double sigma, int maxIterations)
