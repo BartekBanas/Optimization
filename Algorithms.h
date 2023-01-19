bfc5ca526eb130bbc0459402e896338723fe971d
Bartek Banaś <bartek.banas675@gmail.com>
2023-01-19 15:06:46 +0100

    Header changes

--- Algorithms.h
+++ Algorithms.h
@@ -2,9 +2,13 @@
 #include <vector>
 
 std::vector<double> nelderMead(double objectiveFunction(std::vector<double>), const std::vector<double>& initialPoint,
-    double alpha, double gamma, double rho,double sigma, int maxIterations);
+                               double alpha, double gamma, double rho, double sigma, int maxIterations);
+
 std::vector<double> GoldenRatioMethod(double f(std::vector<double>), double A, double B, double epsilon, int nMax);
-std::vector<double> GoldenSectionSearch(double f(std::vector<double>), std::vector<double> a, std::vector<double> b, double epsilon, int nMax);
+std::vector<double> GoldenSectionSearch(double f(std::vector<double>), std::vector<double> a, std::vector<double> b,
+                                        double epsilon, int nMax);
+
 std::vector<double> GradientMethod(std::vector<double> x0, double epsilon, int* fcalls, int nMax);
-std::vector<double> PowellMethod(double f(std::vector<double>), double f2(std::vector<double>), std::vector<double> x0, double a,
-                                 double epsilon, int Nmax);
+
+std::vector<double> PowellMethod(double f(std::vector<double>, double), double f2(std::vector<double>, double),
+                                 std::vector<double> x0, double a, double epsilon, int Nmax);
